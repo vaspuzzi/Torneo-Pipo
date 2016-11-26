5e681f850ba7453a756f41d4da000f2f1e33597c
vaspuzzi <benna1995@hotmail.it>
2016-11-26 19:15:08 +0100

    Add files via upload

---
+++ Torneo/src/AbstractFactory.java
@@ -0,0 +1,8 @@
+
+public interface AbstractFactory {
+	
+	void creaPartecipanteRandom();
+	
+	
+fgd
+}
---
+++ Torneo/src/Partecipante.java
@@ -0,0 +1,21 @@
+
+public class Partecipante {
+	private String squadra;
+	private String nome;
+	private String cognome;
+	private int età;
+
+
+	public Partecipante(String squadra, String nome, String cognome, int età) {
+		this.squadra=squadra;
+		this.nome=nome;
+		this.cognome=cognome;
+		this.età=età;
+	}
+	
+	
+	public Partecipante() {
+		
+	}
+
+}
